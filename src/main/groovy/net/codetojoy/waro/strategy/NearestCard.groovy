05ba5810066b6b61f88ace905c5e8a5c0d2a45d2
Michael Easter <codetojoy@gmail.com>
2013-03-03 09:27:14 -0400

    - min() and max() can accept closures which is truly elegant

--- src/main/groovy/net/codetojoy/waro/strategy/NearestCard.groovy
+++ src/main/groovy/net/codetojoy/waro/strategy/NearestCard.groovy
@@ -3,19 +3,6 @@ package net.codetojoy.waro.strategy
 
 class NearestCard implements Strategy {
     int selectCard(int prizeCard, List<Integer> hand, int maxCard) {
-        def result
-        
-        hand.inject( 1000 * 1000, { diff, card ->
-            def thisDiff = Math.abs(prizeCard - card)
-
-            if (thisDiff < diff) {
-                diff = thisDiff
-                result = card
-            }      
-            
-            diff       
-        } )
-        
-        result
+        hand.min { Math.abs(prizeCard - it) }
     }
 }
